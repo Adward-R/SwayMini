76cfdcf528cd39499e2c45d46dfdfd5b4a5e5b76
Eddie <adward_r@icloud.com>
2015-06-20 22:40:53 +0800

    user bg and django form in sign in page

--- TestApp/user_auth.py
+++ TestApp/user_auth.py
@@ -13,8 +13,30 @@ from django.template.context import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+import time
+from django import forms
 
 from TestApp.models import Published
+
+class SigninForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    passwd = forms.CharField(max_length=20)
+
+    def clean_username(self):
+        _username = self.cleaned_data['username']
+        if not User.objects.filter(username=_username):
+            raise forms.ValidationError('用户不存在')
+        return _username
+
+    def clean(self):
+        cleaned_data = super(SigninForm, self).clean()
+        _username = cleaned_data.get('username', '')
+        _passwd = cleaned_data.get('passwd', '')
+        user = authenticate(username=_username, password=_passwd)
+        if user is None:
+            raise forms.ValidationError('密码错误')
+            #TODO: no passwd wrong info
+        return cleaned_data
 
 def signup(request):
     return render(request, 'signup.html')
@@ -29,23 +51,56 @@ def signed_up(request):
     return HttpResponseRedirect('../home', t.render())
 
 def signin(request):
-    return render(request, 'signin.html')
+    bg_path = os.path.join(os.path.dirname(__file__), '../static/img/bg/')
+    fnames = os.listdir(bg_path)
+    fname = ""
+    while True:
+        idx = int(time.time()) % 7
+        fname = fnames[idx]
+        if fname!=".DS_Store":
+            break
+    form = SigninForm()
+    t = get_template("signin.html")
+    c = RequestContext(request, {"bgFileName": fname, "form": form})
+    return HttpResponse(t.render(c))
 
 def signed_in(request):
-    _username = request.POST['username']
-    _passwd = request.POST['passwd']
-    t = get_template('signup.html')
-    user = authenticate(username=_username, password=_passwd)
-    if user is not None:
-        if user.is_active: #valid, active and authenticated
-            login(request, user)
-            return HttpResponseRedirect('../home/')
-        else:
-            print("User disabled, yet passwd valid") #not likely
+    bg_path = os.path.join(os.path.dirname(__file__), '../static/img/bg/')
+    fnames = os.listdir(bg_path)
+    fname = ""
+    while True:
+        idx = int(time.time()) % 7
+        fname = fnames[idx]
+        if fname!=".DS_Store":
+            break
+
+    form = ""
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
     else:
-        #TODO when login in error occurs
-        print("Info provided does not match")
-    return HttpResponse(str(request.POST))
+        form = SigninForm()
+
+    if form.is_valid(): #has their own exits
+            _username = form.cleaned_data['username']
+            _passwd = form.cleaned_data['passwd']
+            user = authenticate(username=_username, password=_passwd)
+            if user is not None:
+                if user.is_active: #valid, active and authenticated
+                    login(request, user)
+                    return HttpResponseRedirect('../home/') #most likely exit here
+                else:
+                    print("User disabled, yet passwd valid") #not likely
+            else:
+                #TODO when login in error occurs
+                print("Info provided does not match")
+        #return HttpResponse(str(request.POST)) #for debug
+
+    # else, back to login page
+    t = get_template('signin.html')
+    c = RequestContext(request, {'form': form, 'bgFileName': fname})
+
+    return HttpResponse(t.render(c))
+
 
 def signout(request):
     logout(request)
