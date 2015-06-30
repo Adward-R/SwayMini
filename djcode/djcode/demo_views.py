6f6b0c31f13dbe875806c410470f6c38a87042aa
Eddie <adward_r@icloud.com>
2015-07-01 01:26:29 +0800

    ..

--- djcode/djcode/demo_views.py
+++ djcode/djcode/demo_views.py
@@ -3,16 +3,17 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.template.loader import get_template
 from django.template import Context
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.files import File
-import math
 import re
+import os
 from django.template.context import RequestContext
 
 def hello(request):
+        myfile = File(open(str(os.path.dirname(__file__))+'/static/data/log.txt','w'))
+        myfile.write(str(request.POST['changedRegion']))
         return HttpResponse(request.POST)
 
 def home(request):
         return render(request,
-                      'index.html') 
+                      'index.html')
