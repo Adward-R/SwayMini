9393d3e1e834120176a43c76cad1fd63ce236234
Eddie <adward_r@icloud.com>
2015-07-01 01:25:54 +0800

    Revert 923e3b3..e1d5fc0
    This rolls back to commit 923e3b3a960d8ebb7ba964909568c9b910f7f92b.

--- SwayMini/TestApp/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-class Published(models.Model):
-    doc_id = models.CharField(max_length=10, primary_key=True)
-    doc_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=20)
-    isPublic = models.BooleanField(default=False)
---
+++ djcode/djcode/demo_views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.files import File
+import math
+import re
+from django.template.context import RequestContext
+
+def hello(request):
+        return HttpResponse(request.POST)
+
+def home(request):
+        return render(request,
+                      'index.html') 
